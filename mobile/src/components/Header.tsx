cd55df32b6c6218c9cffd72ba90704f4d6346f6f
RafaRamosCosta <rm94281@fiap.com.br>
2023-01-18 15:39:52 -0300

    refactor: changes activeOpacity.

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -9,7 +9,7 @@ export function Header() {
       <Logo />
 
       <TouchableOpacity
-        activeOpacity={0.4}
+        activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
       >
         <Feather name="plus" color={colors.violet[500]} size={20} />
