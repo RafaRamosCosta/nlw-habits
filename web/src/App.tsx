ec98d3cc708cae152be99876cc96c0efbde13a7d
RafaRamosCosta <rm94281@fiap.com.br>
2023-01-20 10:24:06 -0300

    chore: adds dayjs config import.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,8 @@
 import { Header } from './components/Header';
 import { SummaryTable } from './components/SummaryTable';
+import './lib/dayjs';
 import './styles/global.css';
-// import { Habit } from './components/Habit';
+
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
